d03dd59eacd5407ab894bf2ffaac770307c46397
allendegroot <allendegroot@mymail.mines.edu>
2020-10-17 11:02:42 -0600

    Functional CV script for Demo 1. Some more intense testing to ensure good results is to occur before the demo, but this fits the bill for the most part.

--- angle_detect.py
+++ angle_detect.py
@@ -12,12 +12,12 @@ xwidth = 54 * (math.pi/180) #radians
 ywidth = 41 * (math.pi/180) #radians
 
 class angle_detection():
-    def __init__():
+    def __init__(self):
         self.initialize_camera()
         self.reported_angle_rads = "No Aruco Found"
-        self.reported_angle_degress = "No Aruco Found"
+        self.reported_angle_degrees = "No Aruco Found"
         self.aruco_found = "No Aruco Found"
-        self.refresh_speed = 2
+        self.refresh_speed = 5
 
     
     def initialize_camera(self):
@@ -42,9 +42,9 @@ class angle_detection():
         
         self.parameters = aruco.DetectorParameters_create()
         
-    def search_aruco:
+    def search_aruco(self):
         self.reported_angle_rads = "No Aruco Found"
-        self.reported_angle_degress = "No Aruco"
+        self.reported_angle_degrees = "No Aruco Found"
         self.aruco_found = "No Aruco Found"
         end_time = time.time() + self.refresh_speed;
         while(time.time() < end_time):
@@ -57,25 +57,44 @@ class angle_detection():
                 self.aruco_found = "Found Aruco!"
                 #Calculate Aruco center and pixel distances to center in x and y directions
                 xcenter = int((corners[0][0][0][0]+corners[0][0][2][0])/2)
+                print("X-center",xcenter)
                 ycenter = int((corners[0][0][0][1]+corners[0][0][2][1])/2)
-                xdist = abs(xcenter - size(image)[0])
-                ydist = abs(ycenter - size(image)[1])
-                xangle = (xdist/size(image)[0]) * xwidth
-                yangle = (ydist/size(image)[0]) * ywidth
+                print("Y-center",ycenter)
+                print("Shape: ", image.shape)
+                xdist = abs(xcenter - image.shape[1]/2)
+                ydist = abs(ycenter - image.shape[0]/2)
+                print("Xdist", xdist)
+                print("Ydist", ydist)
+                xangle = (xdist/image.shape[1]) * xwidth
+                yangle = (ydist/image.shape[0]) * ywidth
+                print("Xangle: ",xangle * 180/math.pi)
+                print("Yangle: ",yangle*180/math.pi)
                 
                 # Calculate the angle from teh z-axis to the center point
                 # First calculate distance (in pixels to screen) on z-axis
-                a1 = xdist/tan(xangle)
-                a2 = ydist/tan(yangle)
+                a1 = xdist/math.tan(xangle)
+                a2 = ydist/math.tan(yangle)
                 a = (a1 + a2)/2
                 #Calculate distance in pixels to image center from screen center as a hypotenuse rather than x and y components
-                d = sqrt(pow(xdist,2) + pow(ydist,2))
+                d = math.sqrt(pow(xdist,2) + pow(ydist,2))
                 #Calculat the angle to z-axis using these two numbersa nd the arctangent relationship.
-                self.reported_angle_rads = atan(d/a)
+                self.reported_angle_rads = math.atan(d/a)
                 self.reported_angle_degrees = self.reported_angle_rads * (180/math.pi)
+                # Start debug
+                print(self.reported_angle_degrees)
+                size = (int(image.shape[1] * .5), int(image.shape[0] * .5))
+                new_img = cv2.resize(image, size)
+                cv2.imshow("Image", image)
+                cv2.imshow("Resized", new_img)
+                cv2.waitKey(0)
+                cv2.destroyAllWindows()
+                # End debug
                 break;
+            self.rawCapture.truncate(0)
                 
                 
+x = angle_detection()
+x.search_aruco()
             
             
             
